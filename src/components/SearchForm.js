d9ed2a14e7c28719ec832e451136077841262002
Abraham <amwaura101@gmail.com>
2021-03-13 23:06:24 +0300

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {useGlobalcontext} from "../context"
+
+export default function SearchForm() {
+    const {setSearchTerm} = useGlobalcontext() // destructuring the function
+    const searchvalue = React.useRef("")
+
+    React.useEffect(()=> {
+        searchvalue.current.focus()
+    }, [])
+
+    function searchCocktail(){
+        setSearchTerm(searchvalue.current.value)
+    }
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
+    return (
+        <section className = "section search">
+            <form className = "search-form" onSubmit = {handleSubmit}>
+                <div className = 'form-control'>
+                    <label htmlFor ="name"> search your favorite cocktail</label>
+                    <input 
+                        type= "text"
+                        name= "name"
+                        id="name"
+                        ref={searchvalue}
+                        onChange={searchCocktail}
+                    />
+                
+                </div>
+            </form>
+        </section>
+    )
+}
